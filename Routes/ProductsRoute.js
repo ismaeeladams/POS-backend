2449c5f99b79407b8303ff8626839cd06e9a0ef6
ismaeeladams <adamsismaeel387@gmail.com>
2022-08-10 16:00:22 +0200

    host on heroku

--- Routes/ProductsRoute.js
+++ Routes/ProductsRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const con = require("../lib/db_connection");
 const jwt = require("jsonwebtoken");
-const middleware = require("../middleware/auth");
+const middleware = require("../Middleware/auth");
 
 // Get All Products
 router.get("/", (req, res) => {
