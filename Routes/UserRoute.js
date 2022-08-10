b9bb7b3460c494a20986348c4a293da31037bcb6
ismaeeladams <adamsismaeel387@gmail.com>
2022-08-10 09:55:20 +0200

    first commit

---
+++ Routes/UserRoute.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+const con = require("../lib/db_connection");
+const jwt = require("jsonwebtoken");
+
+// Get All Users
+router.get("/", (req, res) => {
+  try {
+    con.query("SELECT * FROM users", (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+
+// Gets one users
+router.get("/:id", (req, res) => {
+  try {
+    con.query(
+      `SELECT * FROM users WHERE user_id = ${req.params.id}`,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+    // res.send({ id: req.params.id });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+
+// Encryption
+const bcrypt = require("bcryptjs");
+
+// Verify
+router.get("/users/verify", (req, res) => {
+  const token = req.header("x-auth-token");
+  jwt.verify(token, process.env.jwtSecret, (error, decodedToken) => {
+    if (error) {
+      res.status(401).json({
+        msg: "Unauthorized Access!",
+      });
+    } else {
+      res.status(200);
+      res.send(decodedToken);
+    }
+  });
+});
